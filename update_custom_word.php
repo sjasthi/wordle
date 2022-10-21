ed72ddb63644626de5af892aaa6a97d241e8b9fc
Julia Ha <juliaha0902@gmail.com>
2022-10-21 00:43:04 -0500

    Revert "Merge CSS file & Fixing Responsive on mobile device"
    This reverts commit 0dc208ca6b7dface6b635f3dbf12e6e5b3ed3e26.

--- update_custom_word.php
+++ update_custom_word.php
@@ -25,7 +25,7 @@ if (isset($_GET['id'])){
     <title>Custom Words Table</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/custom_page.css">
     <link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
     <script src="js/animals.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
